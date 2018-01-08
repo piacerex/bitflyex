3f415d061d218e33c5c0fdf546a3de6dd6eebc97
piacere-ex <masq_wise@hotmail.com>
2018-01-09 01:02:18 +0900

    Refactor json call to smart (Merged header function. As a result, removed path function)

--- lib/bitflyer.ex
+++ lib/bitflyer.ex
@@ -11,28 +11,45 @@ defmodule BitFlyex do
 	defp to_side(  1 ), do: "BUY"
 	defp to_side( -1 ), do: "SELL"
 
+	defp get_public( path, map_function \\ &nop/1 ) do
+		Json.get( 
+			domain(), 
+			path,
+			[],
+			map_function )
+	end
+	defp get_private( path, map_function \\ &nop/1 ) do
+		Json.get( 
+			domain(), 
+			path, 
+			[ 
+				"ACCESS-KEY":       "#{api_key()}", 
+				"ACCESS-TIMESTAMP": "#{timestamp()}", 
+				"ACCESS-SIGN":      "#{sign( "GET", path )}", 
+				"Content-type":     "application/json", 
+			], 
+			map_function )
+	end
+	defp post_private( path, body, map_function \\ &nop/1 ) do
+		Json.post( 
+			domain(), 
+			path, 
+			body, 
+			[ 
+				"ACCESS-KEY":       "#{api_key()}", 
+				"ACCESS-TIMESTAMP": "#{timestamp()}", 
+				"ACCESS-SIGN":      "#{sign( "POST", path, body )}", 
+				"Content-type":     "application/json", 
+			], 
+			map_function )
+	end
+	defp nop( map_list ), do: map_list
+
 	def timestamp(), do: Dt.now_timestamp( "-", "T", ":", "." ) |> String.slice( 0, 22 )
 	defp sign( method, path, body \\ "" ) do
 		:crypto.hmac( :sha256, secret(), timestamp() <> method <> path <> body ) 
 		|> Base.encode16 
 		|> String.downcase 
-	end
-
-	defp private_post_header( path, body ) do
-		[ 
-			"ACCESS-KEY":       "#{ api_key() }", 
-			"ACCESS-TIMESTAMP": "#{ timestamp() }", 
-			"ACCESS-SIGN":      "#{ sign( "POST", path, body ) }", 
-			"Content-type":     "application/json", 
-		]
-	end
-	defp private_header( path ) do
-		[ 
-			"ACCESS-KEY":       "#{ api_key() }", 
-			"ACCESS-TIMESTAMP": "#{ timestamp() }", 
-			"ACCESS-SIGN":      "#{ sign( "GET", path ) }", 
-			"Content-type":     "application/json", 
-		]
 	end
 
 	@doc """
@@ -42,8 +59,7 @@ defmodule BitFlyex do
 		iex> BitFlyex.markets |> Enum.map( fn %{ "product_code" => product_code } -> product_code end )
 		["BTC_JPY", "FX_BTC_JPY", "ETH_BTC", "BCH_BTC", "BTCJPY12JAN2018", "BTCJPY19JAN2018"]
 	"""
-	def markets(), do: Json.get( domain(), path_markets() )
-	defp path_markets(), do: "/v1/markets"	# This path don't refactoring ('v1' use signature)
+	def markets(), do: get_public( "/v1/markets" )
 
 	@doc """
 	Get board (List bids & asks)
@@ -52,8 +68,7 @@ defmodule BitFlyex do
 		iex> BitFlyex.board |> Map.keys
 		["asks", "bids", "mid_price"]
 	"""
-	def board( product_code \\ nil ), do: Json.get( domain(), path_board() <> params_board( product_code ), private_header( path_board() ) )
-	defp path_board(), do: "/v1/board"	# This path don't refactoring ('v1' use signature)
+	def board( product_code \\ nil ), do: get_public( "/v1/board" <> params_board( product_code ) )
 	defp params_board( nil ), do: ""
 	defp params_board( product_code ), do: "?product_code=#{product_code}"
 
@@ -65,8 +80,7 @@ defmodule BitFlyex do
 		["best_ask", "best_ask_size", "best_bid", "best_bid_size", "ltp","product_code", 
 		 "tick_id", "timestamp", "total_ask_depth", "total_bid_depth", "volume", "volume_by_product"]
 	"""
-	def ticker( product_code \\ nil ), do: Json.get( domain(), path_ticker() <> params_ticker( product_code ), private_header( path_ticker() ) )
-	defp path_ticker(), do: "/v1/ticker"	# This path don't refactoring ('v1' use signature)
+	def ticker( product_code \\ nil ), do: get_public( "/v1/ticker" <> params_ticker( product_code ) )
 	defp params_ticker( nil ), do: ""
 	defp params_ticker( product_code ), do: "?product_code=#{product_code}"
 
@@ -77,8 +91,7 @@ defmodule BitFlyex do
 		iex> BitFlyex.executions |> List.first |> Map.keys
 		["buy_child_order_acceptance_id", "exec_date", "id", "price", "sell_child_order_acceptance_id", "side", "size"]
 	"""
-	def executions( product_code \\ nil ), do: Json.get( domain(), path_executions() <> params_executions( product_code ), private_header( path_executions() ) )
-	defp path_executions(), do: "/v1/executions"	# This path don't refactoring ('v1' use signature)
+	def executions( product_code \\ nil ), do: get_public( "/v1/executions" <> params_executions( product_code ) )
 	defp params_executions( nil ), do: "?count=1000"
 	defp params_executions( product_code ), do: "?count=1000&product_code=#{product_code}"
 
@@ -89,8 +102,7 @@ defmodule BitFlyex do
 		iex> BitFlyex.get_permisions |> is_list
 		true
 	"""
-	def get_permisions(), do: Json.get( domain(), path_get_permisions(), private_header( path_get_permisions() ) )
-	defp path_get_permisions(), do: "/v1/me/getpermissions"	# This path don't refactoring ('v1' use signature)
+	def get_permisions(), do: get_private( "/v1/me/getpermissions" )
 
 	@doc """
 	List account balance
@@ -99,8 +111,7 @@ defmodule BitFlyex do
 		iex> BitFlyex.balance |> is_list
 		true
 	"""
-	def balance(), do: Json.get( domain(), path_balance(), private_header( path_balance() ), &map_balance/1 )
-	defp path_balance(), do: "/v1/me/getbalance"	# This path don't refactoring ('v1' use signature)
+	def balance(), do: get_private( "/v1/me/getbalance", &map_balance/1 )
 	defp map_balance( map_list ) do
 		map_list
 		|> Enum.filter( fn %{ "currency_code" => currency_code } -> currency_code != "JPY" end )
@@ -113,8 +124,7 @@ defmodule BitFlyex do
 		iex> BitFlyex.get_collateral |> Map.keys
 		["collateral", "keep_rate", "open_position_pnl", "require_collateral"]
 	"""
-	def get_collateral(), do: Json.get( domain(), path_get_collateral(), private_header( path_get_collateral() ) )
-	defp path_get_collateral(), do: "/v1/me/getcollateral"	# This path don't refactoring ('v1' use signature)
+	def get_collateral(), do: get_private( "/v1/me/getcollateral" )
 
 	@doc """
 	Get collateral acounts
@@ -123,8 +133,7 @@ defmodule BitFlyex do
 		iex> BitFlyex.get_collateral_accounts
 		[]
 	"""
-	def get_collateral_accounts(), do: Json.get( domain(), path_get_collateral_accounts(), private_header( path_get_collateral_accounts() ) )
-	defp path_get_collateral_accounts(), do: "/v1/me/getcollateralaccounts"	# This path don't refactoring ('v1' use signature)
+	def get_collateral_accounts(), do: get_private( "/v1/me/getcollateralaccounts" )
 
 	@doc """
 	List bitcoin addresses
@@ -135,8 +144,7 @@ defmodule BitFlyex do
 		iex> BitFlyex.get_addresses |> Enum.map( fn %{ "currency_code" => currency_code } -> currency_code end )
 		["BTC", "BCH"]
 	"""
-	def get_addresses(), do: Json.get( domain(), path_get_addresses(), private_header( path_get_addresses() ) )
-	defp path_get_addresses(), do: "/v1/me/getaddresses"	# This path don't refactoring ('v1' use signature)
+	def get_addresses(), do: get_private( "/v1/me/getaddresses" )
 
 	@doc """
 	Get bitcoin ins
@@ -145,8 +153,7 @@ defmodule BitFlyex do
 		iex> BitFlyex.get_coin_ins
 		[]
 	"""
-	def get_coin_ins(), do: Json.get( domain(), path_get_coin_ins(), private_header( path_get_coin_ins() ) )
-	defp path_get_coin_ins(), do: "/v1/me/getcoinins"	# This path don't refactoring ('v1' use signature)
+	def get_coin_ins(), do: get_private( "/v1/me/getcoinins" )
 
 	@doc """
 	Get bitcoin outs
@@ -155,8 +162,7 @@ defmodule BitFlyex do
 		iex> BitFlyex.get_coin_outs
 		[]
 	"""
-	def get_coin_outs(), do: Json.get( domain(), path_get_coin_outs(), private_header( path_get_coin_outs() ) )
-	defp path_get_coin_outs(), do: "/v1/me/getcoinouts"	# This path don't refactoring ('v1' use signature)
+	def get_coin_outs(), do: get_private( "/v1/me/getcoinouts" )
 
 	@doc """
 	Get bank accounts
@@ -165,8 +171,7 @@ defmodule BitFlyex do
 		iex> BitFlyex.get_bank_accounts |> List.first |> Map.keys
 		["account_name", "account_number", "account_type", "bank_name", "branch_name", "id", "is_verified"]
 	"""
-	def get_bank_accounts(), do: Json.get( domain(), path_get_bank_accounts(), private_header( path_get_bank_accounts() ) )
-	defp path_get_bank_accounts(), do: "/v1/me/getbankaccounts"	# This path don't refactoring ('v1' use signature)
+	def get_bank_accounts(), do: get_private( "/v1/me/getbankaccounts" )
 
 	@doc """
 	List deposits
@@ -175,8 +180,7 @@ defmodule BitFlyex do
 		iex> BitFlyex.get_deposits |> is_list
 		true
 	"""
-	def get_deposits(), do: Json.get( domain(), path_get_deposits(), private_header( path_get_deposits() ) )
-	defp path_get_deposits(), do: "/v1/me/getdeposits"	# This path don't refactoring ('v1' use signature)
+	def get_deposits(), do: get_private( "/v1/me/getdeposits" )
 
 	@doc """
 	Withdraw to bank
@@ -186,10 +190,8 @@ defmodule BitFlyex do
 #		%{"message_id" => "2bxxxxx-exxx-4xxx-bxxx-c8xxxxxxxx"}
 	"""
 	def withdraw( currency_code, bank_account_id, amount, code ) do
-		Json.post( domain(), path_withdraw(), params_withdraw( currency_code, bank_account_id, amount, code ), 
-			private_post_header( path_withdraw(), params_withdraw( currency_code, bank_account_id, amount, code ) ) )
+		post_private( "/v1/me/withdraw", params_withdraw( currency_code, bank_account_id, amount, code ) )
 	end
-	defp path_withdraw(), do: "/v1/me/withdraw"	# This path don't refactoring ('v1' use signature)
 	defp params_withdraw( currency_code, bank_account_id, amount, code ) do
 		"{ " <> 
 			"\"currency_code\":   \"#{currency_code}\", "   <> 
@@ -206,8 +208,7 @@ defmodule BitFlyex do
 		iex> BitFlyex.get_withdrawals |> is_list
 		true
 	"""
-	def get_withdrawals(), do: Json.get( domain(), path_get_withdrawals(), private_header( path_get_withdrawals() ) )
-	defp path_get_withdrawals(), do: "/v1/me/getwithdrawals"	# This path don't refactoring ('v1' use signature)
+	def get_withdrawals(), do: get_private( "/v1/me/getwithdrawals" )
 
 #	@doc """
 #	Get trading commission
@@ -216,8 +217,7 @@ defmodule BitFlyex do
 #		iex> BitFlyex.get_trading_commission |> Map.keys
 #		["buy_child_order_acceptance_id", "exec_date", "id", "price", "sell_child_order_acceptance_id", "side", "size"]
 #	"""
-#	def get_trading_commission( product_code ), do: Json.get( domain(), path_get_trading_commission() <> params_get_trading_commission( product_code ), private_header( path_get_trading_commission() ) )
-#	defp path_get_trading_commission(), do: "/v1/me/gettradingcommission"	# This path don't refactoring ('v1' use signature)
+#	def get_trading_commission( product_code ), do: get_private( "/v1/me/gettradingcommission" <> params_get_trading_commission( product_code ) )
 #	defp params_get_trading_commission( product_code ), do: "?product_code=#{product_code}"
 
 	@doc """
@@ -230,10 +230,9 @@ defmodule BitFlyex do
 #		%{"child_order_acceptance_id" => "JRF20180108-125324-357807"}
 	"""
 	def order_market( product_code, signal, position_size ) do
-		params = params_send_child_order( product_code, "MARKET", to_side( signal ), 0, position_size, 43200, "GTC" )
-		Json.post( domain(), path_send_child_order(), params,  private_post_header( path_send_child_order(), params ) )
+		post_private( "/v1/me/sendchildorder", 
+			params_send_child_order( product_code, "MARKET", to_side( signal ), 0, position_size, 43200, "GTC" ) )
 	end
-	defp path_send_child_order(), do: "/v1/me/sendchildorder"	# This path don't refactoring ('v1' use signature)
 	defp params_send_child_order( product_code, child_order_type, side, price, size, minute_to_expire, time_in_force ) do
 		"{ " <> 
 			"\"product_code\":     \"#{product_code}\", "     <> 
